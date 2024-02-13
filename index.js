f0cc5c7cbae36f1f32139038d1590f93b7324bb8
Pena <penawalin@gmail.com>
2024-02-13 22:53:45 +0000

    Created functions to build the team and generated sample file

--- index.js
+++ index.js
@@ -4,33 +4,33 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const outputPath = './output/team.html';
 const teamMembers = [];
-
 const render = require("./src/page-template.js");
+
+let managerAdded = false;
 
 function promptManager() {
     return inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "Enter the team manager's name..."
+            message: "Enter the team manager's name:"
         },
         {
             type: "input",
             name: "id",
-            message: "Enter the team manager's employee ID..."
+            message: "Enter the team manager's employee ID:"
         },
         {
             type: "input",
             name: "email",
-            message: "Enter the team manager's email address..."
+            message: "Enter the team manager's email address:"
         },
         {
             type: "input",
             name: "officeNumber",
-            message: "Enter the team manager's office number..."
+            message: "Enter the team manager's office number:"
         }
     ]);
 }
@@ -40,22 +40,22 @@ function promptEngineer() {
         {
             type: "input",
             name: "name",
-            message: "Enter the engineer's name..."
+            message: "Enter the engineer's name:"
         },
         {
             type: "input",
             name: "id",
-            message: "Enter the engineer's employee ID..."
+            message: "Enter the engineer's employee ID:"
         },
         {
             type: "input",
             name: "email",
-            message: "Enter the engineer's email address..."
+            message: "Enter the engineer's email address:"
         },
         {
             type: "input",
             name: "github",
-            message: "Enter the engineer's GitHub username..."
+            message: "Enter the engineer's GitHub username:"
         }
     ]);
 }
@@ -65,22 +65,94 @@ function promptIntern() {
         {
             type: "input",
             name: "name",
-            message: "Enter the intern's name..."
+            message: "Enter the intern's name:"
         },
         {
             type: "input",
             name: "id",
-            message: "Enter the intern's employee ID..."
+            message: "Enter the intern's employee ID:"
         },
         {
             type: "input",
             name: "email",
-            message: "Enter the intern's email address..."
+            message: "Enter the intern's email address:"
         },
         {
             type: "input",
             name: "school",
-            message: "Enter the intern's school..."
+            message: "Enter the intern's school:"
         }
     ]);
 }
+
+function promptMenu() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'Select an option:',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+        }
+    ]);
+}
+
+function addEngineer() {
+    return promptEngineer()
+        .then(engineer => {
+            const engineerObj = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
+            teamMembers.push(engineerObj);
+            console.log('Engineer added successfully!');
+        });
+}
+
+function addIntern() {
+    return promptIntern()
+        .then(intern => {
+            const internObj = new Intern(intern.name, intern.id, intern.email, intern.school);
+            teamMembers.push(internObj);
+            console.log('Intern added successfully!');
+        });
+}
+
+function buildTeam() {
+    if (!managerAdded) {
+        return promptManager()
+            .then(manager => {
+                const managerObj = new Manager(manager.name, manager.id, manager.email, manager.officeNumber);
+                teamMembers.push(managerObj);
+                managerAdded = true;
+                return buildTeam();
+            })
+            .catch(err => console.log(err));
+    }
+
+    return promptMenu()
+        .then(menu => {
+            switch (menu.option) {
+                case 'Add an engineer':
+                    return addEngineer().then(buildTeam);
+                case 'Add an intern':
+                    return addIntern().then(buildTeam);
+                case 'Finish building the team':
+                    return renderAndSave();
+                default:
+                    console.log('Invalid option');
+                    return buildTeam();
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+function renderAndSave() {
+    const html = render(teamMembers);
+    fs.writeFile(outputPath, html, err => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log('Team HTML file generated successfully:', outputPath);
+        }
+    });
+}
+
+// Start the application
+buildTeam();
--- src/page-template.js
+++ src/page-template.js
@@ -4,8 +4,8 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
+        <div class="my-2 mx-2 card employee-card">
+        <div class="card-header bg-primary text-white">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -23,8 +23,8 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="my-2 mx-2 card employee-card">
+    <div class="card-header bg-primary text-white">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -42,8 +42,8 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="my-2 mx-2 card employee-card">
+    <div class="card-header bg-primary text-white">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -100,14 +100,14 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading bg-danger text-white">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="team-area col-12 d-flex flex-wrap justify-content-center">
                 ${generateTeam(team)}
             </div>
         </div>
